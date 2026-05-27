0189b5fdcdbd9b3df4972f5bce95354dc67c5634
JayrajCE <jayrajsinh.r.barad>
2026-05-27 19:49:59 +0530

    Initial commit

---
+++ models/userSchema.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true,
+    },
+    passsword : {
+        type : String , 
+        enum :['admin' , 'employee'],
+        required : true
+    },
+    profileImage : {
+        type : String,
+    },
+    createdAt : {
+        type : Date,
+        default : Date.now
+    },
+    updatedAt : {
+        type : Date,
+        default : Date.now
+    }
+})
+
+const User = mongoose.model("User" , userSchema);
+
+module.exports = {User} 
