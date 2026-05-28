0e163349e198431a20aeb5d49074f031b0c9a935
KunjThummar <kunjthummar.ce@gmail.com>
2026-05-28 10:39:34 +0530

    append fetchdepartment code

---
+++ controllers/departmentFetch.js
@@ -0,0 +1,14 @@
+const { User } = require('../models/userSchema')
+const { Department } = require('../models/departmentSchema')
+
+
+async function fetchDepartments(req, res) {
+    try {
+        const departments = await Department.find();
+        return res.status(200).json({ departments });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+module.exports = { fetchDepartments };
--- routes/route.js
+++ routes/route.js
@@ -6,8 +6,11 @@ const router = express.Router();
 const { register } = require('../controllers/userRegister')
 
 const { login } = require('../controllers/userLogin')
+
+const {fetchDepartments} = require('../controllers/departmentFetch')
     
 router.post('/register' , register )
 router.post('/login', login)
+router.get('/departments', fetchDepartments)
 
 module.exports = router;
